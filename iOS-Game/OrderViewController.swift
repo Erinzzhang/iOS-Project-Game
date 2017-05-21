40c0a6409ff1b1aec8f349675d01e7e379b7afaa
ErinZhang <b10231034@gmail.com>
2017-05-21 13:33:40 +0800

    Add files via upload

---
+++ iOS-Game/OrderViewController.swift
@@ -0,0 +1,41 @@
+//
+//  OrderViewController.swift
+//  iOS-Game
+//
+//  Created by erin on 2017/5/21.
+//  Copyright © 2017年 Erin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+    
+    @IBAction func BackButton(_ sender: UIBarButtonItem) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "PhotoViewController"){
+            show(vc,sender: self)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
